311445b6c6b07f6f0124eb17b34de238e19e913e
Liwei Lu <ricardoluliwei@gmail.com>
2022-01-26 21:57:30 -0800

    Week1 correct

--- week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
+++ week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -1,27 +1,29 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 int64_t MaxPairwiseProduct(const std::vector<int>& numbers) {
-    int max_num = -1;
-    int second_max_num = -1;
-    for (int number: numbers){
-      if (number >= max_num){
-        second_max_num = max_num;
-        max_num = number;
-      }
+  int max_num = -1;
+  int second_max_num = -1;
+  for (int number : numbers) {
+    if (number >= max_num) {
+      second_max_num = max_num;
+      max_num = number;
+    } else if (number > second_max_num) {
+      second_max_num = number;
     }
-    return static_cast<int64_t>(max_num) * second_max_num;
+  }
+  return static_cast<int64_t>(max_num) * second_max_num;
 }
 
 int main() {
-    int n;
-    std::cin >> n;
-    std::vector<int> numbers(n);
-    for (int i = 0; i < n; ++i) {
-        std::cin >> numbers[i];
-    }
+  int n;
+  std::cin >> n;
+  std::vector<int> numbers(n);
+  for (int i = 0; i < n; ++i) {
+    std::cin >> numbers[i];
+  }
 
-    std::cout << MaxPairwiseProduct(numbers) << "\n";
-    return 0;
+  std::cout << MaxPairwiseProduct(numbers) << "\n";
+  return 0;
 }
